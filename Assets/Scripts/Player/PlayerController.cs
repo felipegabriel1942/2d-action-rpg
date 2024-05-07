02ab348204c48365d3987a01296d3809cb9dc12a
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2024-05-07 12:29:36 -0300

    Criando animação de andar ao se mover para direita

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,10 +10,12 @@ public class PlayerController : MonoBehaviour
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator myAnimator;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
         playerControls = new PlayerControls();
+        myAnimator = GetComponent<Animator>();
     }
 
 
@@ -29,6 +31,7 @@ public class PlayerController : MonoBehaviour
 
     private void PlayerInput() {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+        myAnimator.SetFloat("moveX", movement.x);
     }
 
     private void FixedUpdate() {
